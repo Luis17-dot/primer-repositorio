3d263a5c87eec274558923dd071750cd14510143
Luis García <luisgarhig@gmail.com>
2025-10-03 13:45:55 +0200

    usar fetch

--- codigo.py
+++ codigo.py
@@ -1,5 +1,5 @@
 nombre = "Luisi Cambio x2"
 apellido = "García"
 
-saludo= f"Hola soy, {nombre} {apellido}. ¡Bienvenido! ¿Cómo estás?"
+saludo= f"Hola soy, {nombre} {apellido}. ¡Bienvenido! ¿Cómo estás? Es un cambio para el fetch"
 print(saludo)
